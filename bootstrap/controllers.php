8386f88b5ed75138f19d85d983e9618233e72dae
keshan <stkeshansandeepa@gmail.com>
2020-06-02 01:23:24 +0530

    Updated Structure

--- bootstrap/controllers.php
+++ bootstrap/controllers.php
@@ -1,20 +1,11 @@
 <?php
 
-use App\Controllers\HomeController;
-use App\Controllers\Auth\LoginController;
-use App\Controllers\Auth\SignOutController;
-use App\Controllers\Patient\PatientsController;
-use App\Controllers\Hospital\HospitalController;
-use App\Controllers\ICU\IcuController;
-use App\Controllers\Wards\WardController;
-use App\Controllers\ICU\IcuTransferController;
-use  App\Controllers\ManageStaff;
-use App\Controllers\Administrative\AdministrativeController;
-
-$container->add(HomeController::class,function () use($container){
-   return new HomeController(
-       $container->get('view')
-   );
-});
 
 
+//$container->add(HomeController::class,function () use($container){
+//   return new HomeController(
+//       $container->get('view')
+//   );
+//});
+//
+//
